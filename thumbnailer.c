d995d1c634efd0976efc08d59a712a654eb4d9b9
max <dayvson@gmail.com>
2012-02-07 16:50:49 -0200

    Add the first steps to load video using avcodec/avformat

---
+++ thumbnailer.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) Maxwell Dayvson <dayvson@gmail.com>
+ * Copyright (c) Tiago de Pádua <tiagopadua@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+
+
+static void
+error (const char *msg)
+{
+  fprintf (stderr, msg);
+  fprintf (stderr, "\n");
+  exit (1);
+}
+
+
+int
+tve_open_video (const char *fname)
+{
+  AVFormatContext *format_ctx;
+  AVCodecContext *codec_ctx;
+  AVCodec *codec;
+  size_t i;
+  int videostream;
+
+  if (avformat_open_input(&format_ctx, fname, NULL, NULL) != 0)
+    {
+      error("avformat_open_input() has failed");
+    }
+  if (av_find_stream_info(format_ctx) < 0)
+    {
+      error("av_find_stream_info() has failed");
+    }
+
+  videostream = -1;
+  for (i = 0; i < format_ctx->nb_streams; i++)
+    {
+      if (format_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+          videostream = i;
+          break;
+        }
+    }
+
+  if (videostream == -1)
+    error("videostream not found");
+
+  codec_ctx = format_ctx->streams[videostream]->codec;
+  if ((codec = avcodec_find_decoder(codec_ctx->codec_id)) == NULL)
+    error("codec not found");
+  if (avcodec_open (codec_ctx, codec) < 0)
+    error("unable to open codec");
+
+  return 0;
+}
